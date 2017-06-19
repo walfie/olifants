00eff78fe0007011851334753e7ec7c60d32ccc7
Walfie <walfington@gmail.com>
2017-06-18 21:13:50 -0400

    Fix nullable API fields

--- src/api.rs
+++ src/api.rs
@@ -1,29 +1,36 @@
+pub type StatusId = i64;
+pub type AccountId = i64;
+pub type MentionId = i64;
+pub type AttachmentId = i64;
+
 #[derive(Clone, Debug, Default, Deserialize, PartialEq, Serialize)]
 pub struct Status {
-    pub id: i64,
-    pub created_at: String,
-    pub in_reply_to_id: Option<i64>,
-    pub in_reply_to_account_id: Option<i64>,
-    pub sensitive: Option<bool>,
-    pub spoiler_text: String,
-    pub visibility: String,
-    pub language: String,
-    pub application: Option<Application>,
-    pub account: Account,
-    pub media_attachments: Vec<MediaAttachment>,
-    pub mentions: Vec<Mention>,
-    pub tags: Vec<Tag>,
+    pub id: StatusId,
     pub uri: String,
-    pub content: String,
     pub url: String,
+    pub account: Account,
+    pub in_reply_to_id: Option<StatusId>,
+    pub in_reply_to_account_id: Option<AccountId>,
+    pub reblog: Option<Box<Status>>,
+    pub content: String,
+    pub created_at: String,
     pub reblogs_count: i32,
     pub favourites_count: i32,
-    pub reblog: Option<Box<Status>>,
+    pub reblogged: Option<bool>,
+    pub favourited: Option<bool>,
+    pub sensitive: Option<bool>,
+    pub spoiler_text: String,
+    pub visibility: String, // TODO: Enum
+    pub media_attachments: Vec<Attachment>,
+    pub mentions: Vec<Mention>,
+    pub tags: Vec<Tag>,
+    pub application: Option<Application>,
+    pub language: String,
 }
 
 #[derive(Clone, Debug, Default, Deserialize, PartialEq, Serialize)]
 pub struct Mention {
-    pub id: i32,
+    pub id: MentionId,
     pub url: String,
     pub username: String,
     pub acct: String,
@@ -43,7 +50,7 @@ pub struct Application {
 
 #[derive(Clone, Debug, Default, Deserialize, PartialEq, Serialize)]
 pub struct Account {
-    pub id: i64,
+    pub id: AccountId,
     pub username: String,
     pub acct: String,
     pub display_name: String,
@@ -61,12 +68,22 @@ pub struct Account {
 }
 
 #[derive(Clone, Debug, Default, Deserialize, PartialEq, Serialize)]
-pub struct MediaAttachment {
-    pub id: i64,
-    pub remote_url: String,
+pub struct Attachment {
+    pub id: AttachmentId,
     #[serde(rename = "type")]
     pub media_type: String,
     pub url: String,
+    pub remote_url: Option<String>,
     pub preview_url: String,
     pub text_url: Option<String>, // TODO: Add meta (dimensions, etc)
+}
+
+#[derive(Clone, Debug, Default, Deserialize, PartialEq, Serialize)]
+pub struct Notification {
+    pub id: i64,
+    #[serde(rename = "type")]
+    pub notification_type: String,
+    pub created_at: String,
+    pub account: Account,
+    pub status: Option<Status>,
 }
